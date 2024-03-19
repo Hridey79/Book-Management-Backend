c3d5eefa3bae3d64411534729b6f86867cb73080
HrideyVerma79 <82920883+HrideyVerma79@users.noreply.github.com>
2024-03-19 14:27:05 +0530

    book schema created

---
+++ server/schema/schema.js
@@ -0,0 +1,38 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+var books = [
+    { name: 'Harry Potter', genre: 'Fantasy', id: '1' },
+    { name: 'Lord of the Rings', genre: 'Fantasy', id: '2' },
+    { name: 'A Berief History of Time', genre: 'Sci-Fi', id: '3' },
+];
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: ( ) => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args){
+                // code to get data from db / other source
+                return _.find(books, { id: args.id });
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
